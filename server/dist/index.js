0c76590494bbe3c3084fe992d87e4552f5f04c63
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-10-13 13:37:37 +0300

    fixed localhost

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.get('/', (req, res) => {
     res.send('Hello from TypeScript Express server!');
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from TypeScript Express server!');
